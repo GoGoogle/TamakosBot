ad4e511c9cf7b3c70f3062b05cf4eee27cedcee3
lemos1235 <xfe1235@gmail.com>
2017-10-26 19:23:42 +0800

    6th

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ bot.on('message', (msg) => {
 
   var hi = "hi";
   if (msg.text.toLowerCase().indexOf(hi) === 0) {
-    bot.sendMessage(msg.from.id, "Hello " + msg.from.first_name + "~");
+    bot.sendMessage(msg.from.id, "Hi " + msg.from.first_name + "~");
   }
 
   var bye = "bye";
