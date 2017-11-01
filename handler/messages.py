a8176a46ee7dbf55ede4e90d126d62427f08a02c
lemos1235 <xfe1235@gmail.com>
2017-11-01 16:54:31 +0800

    16th

--- handler/messages.py
+++ handler/messages.py
@@ -24,14 +24,10 @@ def bye_message(bot, update):
 def tks_message(bot, update):
     ha_list = [
         '你们给我搞的这本东西，excited!',
-        '如果说还有一点什么成绩就是军队一律不得经商！',
         '我说另请高明吧！我实在也不是谦虚',
         '这个效率，efficiency',
-        '我今天算是得罪了你们一下！',
-        '如果将来宣传报道有偏差，你们要负责',
         '我今天是作为一个长者来跟你们讲的',
         '我有必要告诉你们一点人参的经验',
-        '中国有一句话叫“闷声大发财”，我就什么话都不说，这是最坠吼的'
     ]
     text = ha_list[random.randint(0, len(ha_list) - 1)]
     logger.info('bye: {}'.format(text))
