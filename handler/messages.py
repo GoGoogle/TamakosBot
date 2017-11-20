0f66f2e07355181fc0846516ec8a08c26454fc13
lemos1235 <xfe1235@gmail.com>
2017-11-20 22:17:49 +0800

    58th

--- handler/messages.py
+++ handler/messages.py
@@ -1,9 +1,11 @@
 import logging
 import random
 
-from telegram.ext import RegexHandler
+from telegram.ext import RegexHandler, CommandHandler, ConversationHandler
 
 logger = logging.getLogger(__name__)
+
+INPUT_USERNAME, INPUT_PASSWORD, CHECK_LOGIN_INFO = range(3)
 
 
 def hi_message(bot, update):
@@ -13,7 +15,7 @@ def hi_message(bot, update):
 
 
 def bye_message(bot, update):
-    text = '很惭愧，就做了一点微小的工作，谢谢大家'
+    text = '拜拜'
     logger.info('bye: {}'.format(text))
     bot.send_message(chat_id=update.message.chat_id, text=text)
 
@@ -21,17 +23,48 @@ def bye_message(bot, update):
 def tks_message(bot, update):
     ha_list = [
         '你们给我搞的这本东西，excited!',
-        '我说另请高明吧！我实在也不是谦虚',
         '这个效率，efficiency',
-        '我今天是作为一个长者来跟你们讲的',
-        '我有必要告诉你们一点人参的经验',
+        '完全没有任何这个意思',
+        '我今天算是得罪了你们一下！'
     ]
     text = ha_list[random.randint(0, len(ha_list) - 1)]
     logger.info('bye: {}'.format(text))
     bot.send_message(chat_id=update.message.chat_id, text=text)
 
 
+def start_login(bot, update):
+    pass
+
+
+def input_username(bot, update):
+    pass
+
+
+def input_password(bot, update):
+    pass
+
+
+def handle_login_information(bot, update):
+    pass
+
+
+def end_login(bot, update):
+    pass
+
+
+conv_handler = ConversationHandler(
+    entry_points=[CommandHandler('login', start_login)],
+    states={
+        INPUT_USERNAME: [],
+        INPUT_PASSWORD: [],
+        # 判断用户密码是否正确？
+        CHECK_LOGIN_INFO: []
+    }
+)
+
+
 def handler_messages(dispatcher):
     dispatcher.add_handler(RegexHandler(r'^(Hi|你好)$', hi_message))
-    dispatcher.add_handler(RegexHandler(r'.*bye$', bye_message))
-    dispatcher.add_handler(RegexHandler(r'.*naive.*', tks_message))
+    dispatcher.add_handler(RegexHandler(r'.*有事$', bye_message))
+    dispatcher.add_handler(RegexHandler(r'.*曰..曰.*', tks_message))
+    dispatcher.add_handler(conv_handler)
