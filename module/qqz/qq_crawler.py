0e3187ac3c4b20ba7ac36768547709671c0c2d08
lemos1235 <xfe1235@gmail.com>
2017-12-03 03:44:34 +0800

    qq 音乐模块基本完成

---
+++ module/qqz/qq_crawler.py
@@ -0,0 +1,186 @@
+import json
+import math
+import re
+import time
+from random import choice, random
+
+import requests
+from requests import RequestException
+
+from config.application import CHUNK_SIZE
+from entity.bot_music import Song, Album, Artist, SongList
+from interface.crawler import CrawlerZ
+from util.encrypt_util import userAgentList
+from util.excep_util import (
+    SongNotAvailable, GetRequestIllegal, exception_handle)
+from util.telegram_util import BotResult
+
+QQ_HEADERS = {
+    'Accept': '*/*',
+    'Accept-Encoding': 'gzip,deflate,sdch',
+    'Accept-Language': 'zh-CN,zh;q=0.8,gl;q=0.6,zh-TW;q=0.4',
+    'Connection': 'keep-alive',
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Referer': 'https://y.qq.com/portal/search.html',
+    'Host': 'c.y.qq.com',
+    'X-Real-IP': '59.111.160.197',
+    'User-Agent': choice(userAgentList)
+}
+
+
+class Crawler(CrawlerZ):
+    def __new__(cls, timeout=120, proxy=None):
+        if not hasattr(cls, 'instance'):
+            cls.instance = super(CrawlerZ, cls).__new__(cls)
+        return cls.instance
+
+    def __init__(self, timeout=120, proxy=None):
+        super().__init__(timeout, proxy)
+        self.session = requests.session()
+        self.session.headers.update(QQ_HEADERS)
+        self.download_session = requests.session()
+
+    @staticmethod
+    def dump_single_song(song, mode=0):
+        artist_list = []
+        for ar in song['singer']:
+            artist_id, artist_name = ar['mid'], ar['name']
+            artist_list.append(Artist(artist_id, artist_name))
+
+        album_id, album_name = song['album']['mid'], song['album']['name']
+        album = Album(album_id, album_name)
+
+        song_id, song_name, song_duration, artists, album = \
+            song['mid'], song['name'], song['interval'], artist_list, album
+        song = Song(song_id, song_name, song_duration, artists, album)
+        return song
+
+    @staticmethod
+    def dump_songs(songs, mode=0):
+        song_list = []
+        for song in songs:
+            song = Crawler.dump_single_song(song, mode)
+            song_list.append(song)
+        return song_list
+
+    @exception_handle
+    def get_request(self, url, params=None, custom_session=None, callback=None):
+        """Send a get request.
+
+        warning: old api.
+        :return: a dict or raise Exception.
+        """
+        if not custom_session:
+            resp = self.session.get(url, params=params, timeout=self.timeout,
+                                    proxies=self.proxies)
+        else:
+            resp = custom_session.get(url, params=params, timeout=self.timeout,
+                                      proxies=self.proxies)
+        if callback:
+            regex_str = r'{}\((.+)\)'.format('callback')
+            data = re.match(regex_str, resp.text).group(1)
+            return json.loads(data)
+        else:
+            result = resp.json()
+            return result
+
+    def parse_guid(self, guid):
+        """
+        解析 url的关键参数
+        :return: {'code':0, 'sip': [], ... , 'key': ''}
+        """
+        url = "https://c.y.qq.com/base/fcgi-bin/fcg_musicexpress.fcg"
+        payload = {
+            'guid': guid,
+            'json': 3,
+            'format': 'json'
+        }
+        result = self.get_request(url, payload)
+        return result
+
+    def search_song(self, song_name, page=1):
+        url = "https://c.y.qq.com/soso/fcgi-bin/client_search_cp"
+        payload = {
+            'w': song_name,
+            'n': 5,
+            'p': page,
+            'aggr': 1,
+            'lossless': 1,
+            'cr': '1',
+            'jsonpCallback': 'callback',
+            'new_json': 1,
+            'format': 'json'
+        }
+        result = self.get_request(url, payload, callback='callback')
+        if result['code'] != 0:
+            self.logger.warning('Song %s search failed! url=%s result=%s', song_name, url, result)
+            return BotResult(400, 'Song {} search failed.'.format(song_name))
+        if result['data']['song']['totalnum'] <= 0:
+            self.logger.warning('Song %s not existed!', song_name)
+            return BotResult(404, 'Song {} not existed.'.format(song_name))
+        else:
+            keyword, song_count, songs = song_name, result['data']['song']['totalnum'], result['data']['songs']['list']
+            songlist = SongList(keyword, song_count, Crawler.dump_songs(songs))
+            return BotResult(200, body=songlist)
+
+    def get_song_detail(self, song_id):
+        url = 'http://c.y.qq.com/v8/fcg-bin/fcg_play_single_song.fcg'
+        payload = {
+            'songmid': song_id,
+            'format': 'json'
+        }
+        try:
+            result = self.get_request(url, payload)
+            song = result['data'][0]
+            single_song = Crawler.dump_single_song(song)
+            file = result['data'][0]['file']
+            if file['size_320mp3']:
+                type_o = ('M800', 'mp3')
+            elif file['size_128mp3']:
+                type_o = ('M500', 'mp3')
+            elif file['size_96aac']:
+                type_o = ('C400', 'm4a')
+            else:
+                type_o = ('C200', 'm4a')
+            url = self.get_song_url(song_id, type_o)
+            single_song.song_url = url
+            return BotResult(200, body=single_song)
+        except (SongNotAvailable, GetRequestIllegal, RequestException) as e:
+            self.logger.warning('Song %s get detail error', song_id)
+            return BotResult(400, 'Return {0} when try to get {1} => {2}'.format(e, url, payload))
+
+    def get_song_url(self, song_id, type_o=None):
+        guid = math.floor(random.random() * 1000000000)
+        secret_token = self.parse_guid(guid)
+        base, prefix, extension, vkey, guid, fromtag = \
+            secret_token['sip'][0], type_o[0], type_o[1], secret_token['key'], guid, 30
+        song_url = "{0}{1}{2}.{3}?vkey={3}&guid={4}&fromtag={5}".format(
+            base, prefix, song_id, extension, vkey, guid, fromtag)
+        return song_url
+
+    @exception_handle
+    def write_file(self, songfile, handle=None):
+        resp = self.download_session.get(songfile.file_url, stream=True, timeout=self.timeout)
+        start = time.time()
+        length = int(resp.headers.get('content-length'))
+        dl = 0
+        for chunk in resp.iter_content(CHUNK_SIZE):
+            dl += len(chunk)
+            songfile.file_stream.write(chunk)
+            network_speed = dl / (time.time() - start)
+            if network_speed > 1024 * 1024:
+                network_speed_status = '{:.2f} MB/s'.format(network_speed / (1024 * 1024))
+            else:
+                network_speed_status = '{:.2f} KB/s'.format(network_speed / 1024)
+            if dl > 1024 * 1024:
+                dl_status = '{:.2f} MB'.format(dl / (1024 * 1024))
+            else:
+                dl_status = '{:.0f} KB'.format(dl / 1024)
+            # 已下载大小，总大小，已下载的百分比，网速
+            progress = '{0} / {1:.2f} MB ({2:.0f}%) - {3}'.format(dl_status, length / (1024 * 1024), dl / length * 100,
+                                                                  network_speed_status)
+            if handle:
+                handle.update(progress)
+
+    def login(self, username, password):
+        pass
---
+++ module/qqz/qq_util.py
@@ -0,0 +1,102 @@
+import os
+
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+
+from config import application
+from entity.bot_telegram import ButtonItem, SongFile, SongListSelector
+from interface.util import UtilZ
+
+
+class Util(UtilZ):
+    def __init__(self):
+        super().__init__()
+
+    def get_songlist_selector(self, curpage, songlist):
+        """
+                生成可供选择的歌曲选择器
+                :param curpage: 当前页数
+                :param songlist: 歌曲列表🎵
+                :return: 歌曲列表选择器
+                """
+        self.logger.info('get_songlist_selector: keyword=%s, pagecode=%s', songlist.keyword, curpage)
+        total_page = (songlist.track_count + 4) // 5
+        title = 'qq 关键字「{0}」p: {1}/{2}'.format(songlist.keyword, curpage, total_page)
+        return SongListSelector(title, curpage, total_page, songlist)
+
+    def produce_songlist_panel(self, module_name, songlist_selector):
+        button_list = []
+        # 由于 tg 对 callback_data 字数限制，必须对关键词进行切片
+        songlist_selector.songlist.keyword = songlist_selector.songlist.keyword[:16]
+
+        for x in songlist_selector.songlist.songs:
+            time_fmt = '{0}:{1:0>2d}'.format(int(x.song_duration // 60), int(x.song_duration % 60))
+            button_list.append([
+                InlineKeyboardButton(
+                    text='[{0}] {1} ({2})'.format(
+                        time_fmt, x.song_name, ' / '.join(v.artist_name for v in x.artists)),
+                    callback_data=ButtonItem(module_name, ButtonItem.TYPE_SONGLIST, ButtonItem.OPERATE_SEND,
+                                             x.song_id).dump_json()
+                )
+            ])
+
+        if songlist_selector.total_page == 1:
+            # 什么都不做
+            pass
+        elif songlist_selector.cur_page == 1:
+            button_list.append([
+                InlineKeyboardButton(
+                    text='下一页',
+                    callback_data=ButtonItem(module_name, ButtonItem.TYPE_SONGLIST, ButtonItem.OPERATE_PAGE_DOWN,
+                                             songlist_selector.songlist.keyword,
+                                             songlist_selector.cur_page).dump_json()
+                )
+            ])
+        elif songlist_selector.cur_page == songlist_selector.total_page:
+            button_list.append([
+                InlineKeyboardButton(
+                    text='上一页',
+                    callback_data=ButtonItem(module_name, ButtonItem.TYPE_SONGLIST, ButtonItem.OPERATE_PAGE_UP,
+                                             songlist_selector.songlist.keyword,
+                                             songlist_selector.cur_page).dump_json()
+                )
+            ])
+        else:
+            button_list.append([
+                InlineKeyboardButton(
+                    text='上一页',
+                    callback_data=ButtonItem(module_name, ButtonItem.TYPE_SONGLIST, ButtonItem.OPERATE_PAGE_UP,
+                                             songlist_selector.songlist.keyword,
+                                             songlist_selector.cur_page).dump_json()
+                ),
+                InlineKeyboardButton(
+                    text='下一页',
+                    callback_data=ButtonItem(module_name, ButtonItem.TYPE_SONGLIST, ButtonItem.OPERATE_PAGE_DOWN,
+                                             songlist_selector.songlist.keyword,
+                                             songlist_selector.cur_page).dump_json()
+                )
+            ])
+        button_list.append([
+            InlineKeyboardButton(
+                text='取消',
+                callback_data=ButtonItem(module_name, ButtonItem.TYPE_SONGLIST, ButtonItem.OPERATE_CANCEL).dump_json()
+            )
+        ])
+
+        return {'text': songlist_selector.title, 'reply_markup': InlineKeyboardMarkup(button_list)}
+
+    def get_songfile(self, song):
+        self.logger.info('get_songfile..')
+
+        start = song.song_url.rindex('.')
+        end = song.song_url.rindex('?')
+        extension = song.song_url[start: end]
+
+        file_name = r'{0} - {1}{2}'.format(
+            song.song_name, ' & '.join(v.artist_name for v in song.artists), extension)
+        self.logger.info('song_name=%s url_extension=%s', song.song_name, extension)
+        file_name = file_name.replace("/", ":")
+        file_path = os.path.join(application.TMP_Folder, file_name)
+        file_url = song.song_url
+        file_stream = open(file_path, 'wb+')
+        song = song
+        return SongFile(file_name, file_path, file_url, file_stream, song)
