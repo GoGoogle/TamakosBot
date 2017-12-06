1d2a4c89011060717aa2036b06ac91a38f54c4ef
lemos1235 <xfe1235@gmail.com>
2017-12-07 01:33:07 +0800

    网速插件优化

--- util/song_util.py
+++ util/song_util.py
@@ -1,11 +1,8 @@
 import taglib
-
 import time
 
-import telegram
-
 from config import application
-from config.application import CHUNK_SIZE, FILE_TRANSFER_TIMEOUT
+from config.application import CHUNK_SIZE
 
 
 def selector_cancel(bot, query):
@@ -25,8 +22,8 @@ def write_id3tags(file_path, song_title, song_artist_list, song_album=None, trac
         song.save()
 
 
-def progress_download(session, songfile, handle):
-    resp = session.get(songfile.file_url, stream=True, timeout=FILE_TRANSFER_TIMEOUT)
+def progress_download(session, songfile, timeout, handle):
+    resp = session.get(songfile.file_url, stream=True, timeout=timeout)
     start = time.time()
     length = int(resp.headers.get('content-length'))
     dl = 0
@@ -35,21 +32,17 @@ def progress_download(session, songfile, handle):
         songfile.file_stream.write(chunk)
         network_speed = dl / (time.time() - start)
         if network_speed > 1024 * 1024:
-            network_speed_status = '{:.0f}M/s'.format(network_speed / (1024 * 1024))
+            network_speed_status = '{:.2f} m/s'.format(network_speed / (1024 * 1024))
         else:
-            network_speed_status = '{:.0f}KB/s'.format(network_speed / 1024)
-        # if dl > 1024 * 1024:
-        #     dl_status = '{:.2f} MB'.format(dl / (1024 * 1024))
-        # else:
-        #     dl_status = '{:.0f} KB'.format(dl / 1024)
-        # 已下载大小，总大小，已下载的百分比，网速
-        # progress = '{0} / {1:.2f} MB ({2:.0f}%) - {3}'.format(dl_status, length / (1024 * 1024),
-        #                                                       dl / length * 100,
-        #                                                       network_speed_status)
-        raw = "»»»»»»»»»»"
-        percent = int(10 - 10 * dl / length)
-        dl_status = raw.replace("+", "-", percent)[::-1]
-        progress = '[{0}] [{1}]'.format(dl_status, network_speed_status)
+            network_speed_status = '{:.2f} kb/s'.format(network_speed / 1024)
+        if dl > 1024 * 1024:
+            dl_status = '{:.2f} MB'.format(dl / (1024 * 1024))
+        else:
+            dl_status = '{:.0f} KB'.format(dl / 1024)
+        progress = '{0} / {1:.2f} MB ({2:.0f}%) - {3}'.format(dl_status, length / (1024 * 1024),
+                                                              dl / length * 100,
+                                                              network_speed_status)
+
         if handle:
             handle.update(progress)
 
