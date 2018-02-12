c60a0bca2d60ee61b98fae08bdc2fc91f6bfdadd
lemos1235 <xfe1235@gmail.com>
2018-02-12 23:28:47 +0800

    update mode

--- module/linkz/link.py
+++ module/linkz/link.py
@@ -11,38 +11,41 @@ class Link(object):
 
     def __init__(self):
         self.logger = logging.getLogger(__name__)
-        self.utilz = link_util.Util()
+        # self.utilz = link_util.Util()
 
     def step_link_pool(self, bot, update):
-        query = update.callback_query
-        self.logger.debug("{} has stepped into link pool".format(query.from_user))
+        pass
+        # query = update.callback_query
+        # self.logger.debug("{} has stepped into link pool".format(query.from_user))
 
-        my_id = query.from_user.id
-        my_message_id = query.message.message_id
+        # my_id = query.from_user.id
+        # my_message_id = query.message.message_id
 
-        self.utilz.line_up(bot, my_id, my_message_id)
+        # self.utilz.line_up(bot, my_id, my_message_id)
 
-        self.utilz.fetch_box(my_id, bot)
+        # self.utilz.fetch_box(my_id, bot)
 
     def leave_link_pool(self, bot, update):
-        query = update.callback_query
-        self.utilz.update_status(self.utilz.Unlink, query.from_user.id)
-        self.utilz.observer_status(bot)  # 自动更新状态
+        pass
+        # query = update.callback_query
+        # self.utilz.update_status(self.utilz.Unlink, query.from_user.id)
+        # self.utilz.observer_status(bot)  # 自动更新状态
 
     def chat_with_partner(self, bot, update):
-        user = self.utilz.get_status(update.message.from_user.id)
-        if user and user["status"] == self.utilz.Linking:
-            partner_id = user["your_id"]
-            bot_msg = BotMessage.get_botmsg(update['message'])
-            if bot_msg.bot_content.text:
-                bot.send_message(chat_id=partner_id, text=bot_msg.bot_content.text)
-            if bot_msg.bot_content.picture.sticker:
-                bot.send_sticker(chat_id=partner_id, sticker=bot_msg.bot_content.picture.sticker)
-            if bot_msg.bot_content.photo:
-                bot.send_photo(chat_id=partner_id, photo=bot_msg.bot_content.photo)
-            if bot_msg.bot_content.audio:
-                bot.send_audio(chat_id=partner_id, audio=bot_msg.bot_content.audio)
-            if bot_msg.bot_content.video:
-                bot.send_video(chat_id=partner_id, video=bot_msg.bot_content.video)
-            if bot_msg.bot_content.document:
-                bot.send_document(chat_id=partner_id, document=bot_msg.bot_content.document)
+        pass
+        # user = self.utilz.get_status(update.message.from_user.id)
+        # if user and user["status"] == self.utilz.Linking:
+        #     partner_id = user["your_id"]
+        #     bot_msg = BotMessage.get_botmsg(update['message'])
+        #     if bot_msg.bot_content.text:
+        #         bot.send_message(chat_id=partner_id, text=bot_msg.bot_content.text)
+        #     if bot_msg.bot_content.picture.sticker:
+        #         bot.send_sticker(chat_id=partner_id, sticker=bot_msg.bot_content.picture.sticker)
+        #     if bot_msg.bot_content.photo:
+        #         bot.send_photo(chat_id=partner_id, photo=bot_msg.bot_content.photo)
+        #     if bot_msg.bot_content.audio:
+        #         bot.send_audio(chat_id=partner_id, audio=bot_msg.bot_content.audio)
+        #     if bot_msg.bot_content.video:
+        #         bot.send_video(chat_id=partner_id, video=bot_msg.bot_content.video)
+        #     if bot_msg.bot_content.document:
+        #         bot.send_document(chat_id=partner_id, document=bot_msg.bot_content.document)
--- module/linkz/link_util.py
+++ module/linkz/link_util.py
@@ -14,119 +14,127 @@ class Util(object):
     UnHandle, Waiting, WaitFor, Response, Linking, Unlink = range(6)  # define status style
 
     def __new__(cls, timeout=120, proxy=None):
-        if not hasattr(cls, 'instance'):
-            cls.instance = super(Util, cls).__new__(cls)
-        return cls.instance
+        pass
+        # if not hasattr(cls, 'instance'):
+        #     cls.instance = super(Util, cls).__new__(cls)
+        # return cls.instance
 
     def __init__(self):
         self.logger = logging.getLogger(__name__)
-        c = MongoClient()
-        db = c.telebot
-        collection = db["user_link"]
-        self.collection = collection
-        self.task_queue = Queue()  # 通过队列不断处理新任务
+        # c = MongoClient()
+        # db = c.telebot
+        # collection = db["user_link"]
+        # self.collection = collection
+        # self.task_queue = Queue()  # 通过队列不断处理新任务
 
     def line_up(self, bot, my_id, my_message_id):
-        self.update_reply_board(bot, my_id, my_message_id, "~", "在吗")
-        self.add_user(my_id, my_message_id)
+        pass
+        # self.update_reply_board(bot, my_id, my_message_id, "~", "在吗")
+        # self.add_user(my_id, my_message_id)
 
     def fetch_box(self, my_id, bot):
-        user = self.collection.find_one({"status": Util.Waiting})
-        if user:
-            # 更新状态为等待回应
-            u_id = user["my_id"]
-            u_msg_id = user["my_message_id"]
-            # print(u_id, 111)
-            # print(my_id, 222)
-            self.update_status(Util.WaitFor, my_id, u_id, u_msg_id)
-            self.observer_status(bot)  # 自动更新状态
-        else:
-            # 更新状态为干等
-            self.update_status(Util.Waiting, my_id)
+        pass
+        # user = self.collection.find_one({"status": Util.Waiting})
+        # if user:
+        #     # 更新状态为等待回应
+        #     u_id = user["my_id"]
+        #     u_msg_id = user["my_message_id"]
+        #     # print(u_id, 111)
+        #     # print(my_id, 222)
+        #     self.update_status(Util.WaitFor, my_id, u_id, u_msg_id)
+        #     self.observer_status(bot)  # 自动更新状态
+        # else:
+        #     # 更新状态为干等
+        #     self.update_status(Util.Waiting, my_id)
 
     def add_user(self, my_id, my_message_id):
-        link_user = {
-            "status": Util.UnHandle,
-            "my_id": my_id,
-            "my_message_id": my_message_id,
-            "your_id": None,
-            "your_message_id": None,
-            "updated": None
-        }
-        self.collection.replace_one({"my_id": my_id}, link_user, upsert=True)
+        pass
+        # link_user = {
+        #     "status": Util.UnHandle,
+        #     "my_id": my_id,
+        #     "my_message_id": my_message_id,
+        #     "your_id": None,
+        #     "your_message_id": None,
+        #     "updated": None
+        # }
+        # self.collection.replace_one({"my_id": my_id}, link_user, upsert=True)
 
     def update_status(self, style, my_id=None, u_id=None, u_msg_id=None):
-        if style == Util.Waiting:
-            self.collection.update_one(
-                {"my_id": my_id}, {"$set": {"status": Util.Waiting, "updated": time.time()}})
-        if style == Util.WaitFor:
-            self.collection.update_one(
-                {"my_id": my_id},
-                {"$set": {
-                         "status": Util.WaitFor, "your_id": u_id, "your_message_id": u_msg_id, "updated": time.time()
-                }})
-        if style == Util.Unlink:
-            _user = self.collection.find_one({"my_id": my_id})
-            if _user["your_id"]:
-                self.collection.update_one(
-                    {"my_id": my_id}, {"$set": {"status": Util.Unlink}})
-                self.collection.update_one(
-                    {"my_id": _user["your_id"]}, {"$set": {"status": Util.Unlink}})
-            else:
-                self.collection.update_one(
-                    {"my_id": my_id}, {"$set": {"status": Util.UnHandle}})
+        pass
+        # if style == Util.Waiting:
+        #     self.collection.update_one(
+        #         {"my_id": my_id}, {"$set": {"status": Util.Waiting, "updated": time.time()}})
+        # if style == Util.WaitFor:
+        #     self.collection.update_one(
+        #         {"my_id": my_id},
+        #         {"$set": {
+        #                  "status": Util.WaitFor, "your_id": u_id, "your_message_id": u_msg_id, "updated": time.time()
+        #         }})
+        # if style == Util.Unlink:
+        #     _user = self.collection.find_one({"my_id": my_id})
+        #     if _user["your_id"]:
+        #         self.collection.update_one(
+        #             {"my_id": my_id}, {"$set": {"status": Util.Unlink}})
+        #         self.collection.update_one(
+        #             {"my_id": _user["your_id"]}, {"$set": {"status": Util.Unlink}})
+        #     else:
+        #         self.collection.update_one(
+        #             {"my_id": my_id}, {"$set": {"status": Util.UnHandle}})
 
     def observer_status(self, bot=None):
-        for waitFor in self.collection.find({"status": Util.WaitFor}):
-            self.collection.update_one(
-                {"my_id": waitFor["your_id"]},
-                {"$set": {
-                    "status": Util.Response, "your_id": waitFor["my_id"], "your_message_id": waitFor["my_message_id"]
-                }})
-        for response in self.collection.find({"status": Util.Response}):
-            self.collection.update_one({"my_id": response["your_id"]}, {"$set": {"status": Util.Linking}})
-            self.collection.update_one({"my_id": response["my_id"]}, {"$set": {"status": Util.Linking}})
+        pass
+        # for waitFor in self.collection.find({"status": Util.WaitFor}):
+        #     self.collection.update_one(
+        #         {"my_id": waitFor["your_id"]},
+        #         {"$set": {
+        #             "status": Util.Response, "your_id": waitFor["my_id"], "your_message_id": waitFor["my_message_id"]
+        #         }})
+        # for response in self.collection.find({"status": Util.Response}):
+        #     self.collection.update_one({"my_id": response["your_id"]}, {"$set": {"status": Util.Linking}})
+        #     self.collection.update_one({"my_id": response["my_id"]}, {"$set": {"status": Util.Linking}})
 
-            self.update_reply_board(bot, response["my_id"], response["my_message_id"], "⦿", "嗯哼")
-            self.update_reply_board(bot, response["your_id"], response["your_message_id"], "⦿", "哈罗")
-        for unlink in self.collection.find({"status": Util.Unlink}):
-            self.collection.update_one(
-                {"my_id": unlink["my_id"]},
-                {"$set": {"status": Util.UnHandle, "your_id": None, "your_message_id": None, "updated": None}})
-            self.update_reply_board(bot, unlink["my_id"], unlink["my_message_id"], "88", "分手")
+        #     self.update_reply_board(bot, response["my_id"], response["my_message_id"], "⦿", "嗯哼")
+        #     self.update_reply_board(bot, response["your_id"], response["your_message_id"], "⦿", "哈罗")
+        # for unlink in self.collection.find({"status": Util.Unlink}):
+        #     self.collection.update_one(
+        #         {"my_id": unlink["my_id"]},
+        #         {"$set": {"status": Util.UnHandle, "your_id": None, "your_message_id": None, "updated": None}})
+        #     self.update_reply_board(bot, unlink["my_id"], unlink["my_message_id"], "88", "分手")
 
     def get_status(self, user_id):
-        user = self.collection.find_one({"my_id": user_id})
-        return user
+        pass
+        # user = self.collection.find_one({"my_id": user_id})
+        # return user
 
     @staticmethod
     def update_reply_board(bot, chat_id, message_id, new_mode_nick, new_mode_text):
-        module_name = "mode"
-        msg_text = "{} 🍐🍐".format(new_mode_text)
-        sel_modelist = ModeList.mode_list
-        button_list = [[InlineKeyboardButton(
-                    text=sel_modelist[0].mode_nick,
-                    callback_data=ButtonItem(module_name, ButtonItem.TYPE_MODE, ButtonItem.OPERATE_SEND,
-                                             sel_modelist[0].mode_id).dump_json()
-                ), InlineKeyboardButton(
-                    text=sel_modelist[1].mode_nick,
-                    callback_data=ButtonItem(module_name, ButtonItem.TYPE_MODE, ButtonItem.OPERATE_SEND,
-                                             sel_modelist[1].mode_id).dump_json()
-                )], [
-                InlineKeyboardButton(
-                    text=sel_modelist[2].mode_nick,
-                    callback_data=ButtonItem(module_name, ButtonItem.TYPE_MODE, ButtonItem.OPERATE_SEND,
-                                             sel_modelist[2].mode_id).dump_json()
-                ),
-                InlineKeyboardButton(
-                    text=sel_modelist[3].mode_nick,
-                    callback_data=ButtonItem(module_name, ButtonItem.TYPE_MODE, ButtonItem.OPERATE_SEND,
-                                             sel_modelist[3].mode_id).dump_json()
-                )], [
-                InlineKeyboardButton(
-                    text=new_mode_nick,
-                    callback_data=ButtonItem(module_name, ButtonItem.TYPE_MODE, ButtonItem.OPERATE_SEND,
-                                             sel_modelist[4].mode_id).dump_json()
-                )]]
-        markup = InlineKeyboardMarkup(button_list)
-        bot.edit_message_text(chat_id=chat_id, message_id=message_id, text=msg_text, reply_markup=markup)
+        pass
+        # module_name = "mode"
+        # msg_text = "{} 🍐🍐".format(new_mode_text)
+        # sel_modelist = ModeList.mode_list
+        # button_list = [[InlineKeyboardButton(
+        #             text=sel_modelist[0].mode_nick,
+        #             callback_data=ButtonItem(module_name, ButtonItem.TYPE_MODE, ButtonItem.OPERATE_SEND,
+        #                                      sel_modelist[0].mode_id).dump_json()
+        #         ), InlineKeyboardButton(
+        #             text=sel_modelist[1].mode_nick,
+        #             callback_data=ButtonItem(module_name, ButtonItem.TYPE_MODE, ButtonItem.OPERATE_SEND,
+        #                                      sel_modelist[1].mode_id).dump_json()
+        #         )], [
+        #         InlineKeyboardButton(
+        #             text=sel_modelist[2].mode_nick,
+        #             callback_data=ButtonItem(module_name, ButtonItem.TYPE_MODE, ButtonItem.OPERATE_SEND,
+        #                                      sel_modelist[2].mode_id).dump_json()
+        #         ),
+        #         InlineKeyboardButton(
+        #             text=sel_modelist[3].mode_nick,
+        #             callback_data=ButtonItem(module_name, ButtonItem.TYPE_MODE, ButtonItem.OPERATE_SEND,
+        #                                      sel_modelist[3].mode_id).dump_json()
+        #         )], [
+        #         InlineKeyboardButton(
+        #             text=new_mode_nick,
+        #             callback_data=ButtonItem(module_name, ButtonItem.TYPE_MODE, ButtonItem.OPERATE_SEND,
+        #                                      sel_modelist[4].mode_id).dump_json()
+        #         )]]
+        # markup = InlineKeyboardMarkup(button_list)
+        # bot.edit_message_text(chat_id=chat_id, message_id=message_id, text=msg_text, reply_markup=markup)
